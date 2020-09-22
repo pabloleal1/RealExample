63f3103cd82538fc8762734470d2e5a7e21d2648
Pablo <pablo.leal.bautista1@gmail.com>
2020-09-22 14:00:06 +0200

    Commited 3

--- front-test-MutuaMad-Git-Example/src/test/java/mutuamad/test/Test_03_07.java
+++ front-test-MutuaMad-Git-Example/src/test/java/mutuamad/test/Test_03_07.java
@@ -68,6 +68,7 @@ public class Test_03_07 extends CreateExtetReport {
                 Utilities.sleep(2000);
                 welcomeScreen.clickButtonReports();
                 System.out.println("Prueba");
+                System.out.println("Prueba");
 
             }
         }
